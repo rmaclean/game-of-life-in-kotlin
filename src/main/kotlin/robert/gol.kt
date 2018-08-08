6f444645d6d37383d15643d1e641376a740c76b0
Robert MacLean <robert@sadev.co.za>
2018-08-08 23:45:58 +0200

    Improvements to talk end state based on dry run

--- src/main/kotlin/robert/gol.kt
+++ src/main/kotlin/robert/gol.kt
@@ -4,9 +4,9 @@ import java.util.Random
 
 const val rows = 20
 const val cols = 90
+val random = Random()
 
-var random = Random()
-var grid = List(rows, { _ -> List(cols, {_ -> random.nextInt(5) == 0})})
+var grid = List(rows, { _ -> List(cols, { _ -> random.nextInt(5) == 0 })})
 
 class Console {
     private fun clear() {
@@ -15,48 +15,49 @@ class Console {
 
     fun render(grid :List<List<Boolean>>) {
         clear()
-        for (row in 0 until rows) {
-            for (col in 0 until cols) {
-                if (grid[row][col]) print('*') else print(' ')
+        // for (int i = 0; i < rows-1; i++){
+        //     row = rows[i]
+        // }
+
+        for(row in 0 until rows) {
+            for(col in 0 until cols) {
+                if (grid[row][col]) {
+                    print("@")
+                } else {
+                    print(" ")
+                }
             }
             println()
-        }    
+        }
     }
 }
 
-data class Point(val x : Int, val y: Int) {
-    fun isAlive() = grid[x][y]
-    fun isValid() = (x in 1 until rows) && (y in 1 until cols)
+data class Point(val row:Int, val col:Int) {
+    fun isAlive(point:Point = this) = grid[point.row][point.col]
+    private fun isValid(point:Point) = point.row in 0 until rows && point.col in 0 until cols
+    fun countNeightbours() = myNeighbours()
+                                .filter{isValid(it)}
+                                .map{isAlive(it)}
+                                .count{it}
 }
 
-fun neighbours(point: Point):Array<Point> {
-    val (x, y) = point;
-    return arrayOf(Point(x-1, y-1), Point(x-1, y), Point(x-1, y+1),
-                   Point(x, y-1),                  Point(x, y+1),  
-                   Point(x+1, y-1), Point(x+1, y), Point(x+1, y+1))  
-} 
-
-fun Point.countNeighbours() = neighbours(this)
-                                .filter{it.isValid()}
-                                .map{it.isAlive()}
-                                .count{it}
+fun Point.myNeighbours() = arrayOf(Point(row-1, col-1), Point(row-1, col), Point(row-1, col +1),
+                                   Point(row, col-1),                      Point(row, col +1),
+                                   Point(row+1, col-1), Point(row+1, col), Point(row+1, col +1))
 
 fun evolve() = (0 until rows).map{row -> 
     (0 until cols).map{col -> 
         val point = Point(row, col)
-        val isAlive = point.isAlive()
-        val neighbours = point.countNeighbours()
-        neighbours == 3 || (isAlive && neighbours == 2) 
-    }    
+        val neighbours = point.countNeightbours()
+        neighbours == 3 || (neighbours == 2 && point.isAlive())
+    }
 }
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>) {  
     val console = Console()
     while (true) {
         console.render(grid) 
         grid = evolve()
         Thread.sleep(200)
     }
-}
-
-
+}   
