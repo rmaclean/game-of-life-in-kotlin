ee92a600793fbebd9493894b8daa4ec572495245
Robert MacLean <robert@sadev.co.za>
2018-08-29 22:21:34 +0200

    First public demo this
    Error message was
    >gradle run
    e: C:\Users\User\Desktop\fbtalk\src\main\kotlin\robert\gol.kt: (36, 24):
    Expecting '('
    e: C:\Users\User\Desktop\fbtalk\src\main\kotlin\robert\gol.kt: (47, 11):
    Expecting '('
    > Task :compileKotlin FAILED
    
    FAILURE: Build failed with an exception.
    
    * What went wrong:
    Execution failed for task ':compileKotlin'.
    > Compilation error. See log for more details
    
    * Try:
    Run with --stacktrace option to get the stack trace. Run with --info or
    --debug option to get more log output. Run with --scan to get full
    insights.
    
    * Get more help at https://help.gradle.org
    
    BUILD FAILED in 2s
    1 actionable task: 1 executed

--- src/main/kotlin/robert/gol.kt
+++ src/main/kotlin/robert/gol.kt
@@ -5,7 +5,8 @@ import java.util.Random
 const val rows = 20
 const val cols = 90
 
-//var grid
+val random = Random()
+var grid = List(rows, {_ -> List(cols, {_ -> random.nextInt(5) == 0})})
 
 class Console {
     private fun clear() {
@@ -14,14 +15,48 @@ class Console {
 
     fun render(grid :List<List<Boolean>>) {
         clear()
-        
+        for (row in 0 until rows) {
+            for (col in 0 until cols) {
+                if (grid[row][col]) {
+                    print('*')
+                } else {
+                    print(" ")
+                }
+            }
+            println()
+        }
     }
 }
+
+data class Point(val row:Int, val col:Int) {
+    fun isAlive(point:Point = this) = grid[point.row][point.col]
+
+    private fun isValid(point: Point) = point.row in 0 until rows && point.col in 0 until cols;
+
+    fun countNeighbours() = myNeighbours(this)
+                            .filter { isValid(it) }
+                            .map { isAlive() }
+                            .count {it}
+}
+
+fun myNeighbours(point: Point) = arrayOf(
+    Point(point.row -1, point.col -1), Point(point.row -1, point.col), Point(point.row -1, point.col +1),
+    Point(point.row, point.col -1),                                     Point(point.row, point.col +1),
+    Point(point.row +1, point.col -1), Point(point.row +1, point.col), Point(point.row +1, point.col +1))
+
+fun evolve = (0 until rows).map{ row ->
+                (0 until cols).map{ col -> 
+                    val point = Point(row, col)
+                    val neighbours = point.countNeighbours()
+                    neighbours == 3 || (neighbours == 2 && point.isAlive())
+                } 
+                }
 
 fun main(args: Array<String>) {
     val console = Console()
     while (true) {
-        //console.render(grid) 
+        console.render(grid) 
+        grid = evolve()
         Thread.sleep(200)
     }
 }
